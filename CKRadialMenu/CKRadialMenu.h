f7176b102d4ab0812901150faf9526e7cd82fe19
Cameron Klein <cameron.klein@gmail.com>
2014-12-07 11:52:31 -0800

    Initial Commit

---
+++ CKRadialMenu/CKRadialMenu.h
@@ -0,0 +1,19 @@
+//
+//  CKRadialMenu.h
+//  CKRadialMenu
+//
+//  Created by Cameron Klein on 12/7/14.
+//  Copyright (c) 2014 Cameron Klein. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CKRadialMenu.
+FOUNDATION_EXPORT double CKRadialMenuVersionNumber;
+
+//! Project version string for CKRadialMenu.
+FOUNDATION_EXPORT const unsigned char CKRadialMenuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CKRadialMenu/PublicHeader.h>
+
+
